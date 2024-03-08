9d8df2c07f971f463740d19f55a4ad42b02af11f
Natalie.Perlin <Natalie.Perlin@noaa.gov>
2024-03-08 20:02:11 +0000

    update modulefiles for the singularity container, config.yaml, plotting tasks

--- modulefiles/wflow_singularity.lua
+++ modulefiles/wflow_singularity.lua
@@ -6,15 +6,17 @@ a singularity container
 whatis([===[Loads libraries needed for running the UFS SRW App in a singularity container]===])
 load("set_pythonpath")
 
-prepend_path("MODULEPATH","/home/ubuntu/rocoto/modulefiles")
-load("rocoto")
+prepend_path("MODULEPATH","/lustre/rocoto/modulefiles")
+load("rocoto/1.3.6")
 
-append_path("MODULEPATH","/home/ubuntu/miniconda3/modulefiles")
+append_path("MODULEPATH","/lustre/miniconda3/modulefiles")
 load("miniconda3")
 
-setenv("PROJ_LIB","/home/ubuntu/miniconda3/4.12.0/envs/regional_workflow/share/proj")
-append_path("PATH","/home/ubuntu/miniconda3/4.12.0/envs/regional_workflow/bin")
+setenv("PROJ_LIB","/lustre/miniconda3/4.12.0/envs/regional_workflow/share/proj")
+append_path("PATH","/lustre/miniconda3/4.12.0/envs/regional_workflow/bin")
 
 if mode() == "load" then
-   execute{cmd="conda activate workflow_tools", modeA={"load"}}
+   LmodMsgRaw([===[Please do the following to activate conda:
+       > conda activate workflow_tools
+]===])
 end
--- srw.sh
+++ srw.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 export SINGULARITYENV_FI_PROVIDER=tcp
-#export SINGULARITY_SHELL=/bin/bash
-export SINGULARITY_SHELL= /usr/share/lmod/lmod/init/bash
-export SINGULARITYENV_PREPEND_PATH="/home/ubuntu/ufs-srweather-app/bin"
-img="/home/ubuntu/ubuntu20.04-intel-srwapp-v2.2.0.img"
+export SINGULARITY_SHELL=/usr/share/lmod/lmod/init/bash
+export SINGULARITYENV_PREPEND_PATH="/lustre/ufs-srweather-app/bin"
+img="/lustre/ubuntu20.04-intel-srwapp-release-public-v2.2.0.img"
 cmd=$(basename "$0")
 arg="$@"
 echo running: singularity exec "${img}" $cmd $arg
-/usr/local/bin/singularity exec -B /home -B /scratch "${img}" $cmd $arg
+/usr/bin/singularity exec -B /home -B /lustre "${img}" $cmd $arg
