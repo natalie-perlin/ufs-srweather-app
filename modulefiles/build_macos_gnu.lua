446d4aeb904b3ab9ffa0f1dd2e780acf799b3b0b
Natalie Perlin <Natalie.Perlin@noaa.gov>
2025-03-10 12:00:46 -0400

    local changes to Macos modulefiles for running an SRW test

--- modulefiles/build_macos_gnu.lua
+++ modulefiles/build_macos_gnu.lua
@@ -9,21 +9,20 @@ if mode() == "load" then
    execute{cmd="ulimit -S -s unlimited", modeA={"load"}}
 end
 
--- Replace the stackpath below by the path of the local spack-stack environment build:
-local stackpath = "/Users/username/spack-stack/spack-stack-1.8.0/envs/ufs-srw-env"
-local modulepath = stackpath .. "/install/modulefiles/Core"
-prepend_path("MODULEPATH", modulepath)
+
+prepend_path("MODULEPATH", "/Users/Natalie/spack-stack/spack-stack-1.9.0/envs/ufs-srw-env/install/modulefiles/Core")
 
 stack_gnu_ver=os.getenv("stack_apple_clang_ver") or "15.0.0"
 load(pathJoin("stack-apple-clang", stack_gnu_ver))
 
-stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "5.0.3"
+stack_openmpi_ver=os.getenv("stack_openmpi_ver") or "5.0.5"
 load(pathJoin("stack-openmpi", stack_openmpi_ver))
 
-cmake_ver=os.getenv("cmake_ver") or "3.27.9"
-load(pathJoin("cmake", cmake_ver))
+-- cmake_ver=os.getenv("cmake_ver") or "3.27.9"
+-- load(pathJoin("cmake", cmake_ver))
+load("cmake")
 
-load("srw_common")
+load("srw_common_macos")
 
 setenv("CC", "mpicc")
 setenv("CXX", "mpicxx")
@@ -42,16 +41,3 @@ setenv("CMAKE_Fortran_COMPILER_ID", "GNU")
 
 setenv("CFLAGS","-Wno-implicit-function-declaration ")
 
-
-local libjpeg_ROOT = os.getenv("libjpeg_turbo_ROOT")
-local jasper_ROOT = os.getenv("jasper_ROOT")
-local libpng_ROOT = os.getenv("libpng_ROOT")
-local ldflags0 = os.getenv("LDFLAGS") or ""
-
-if jasper_ROOT and libpng_ROOT and libjpeg_ROOT then
-   local ldflags1 = " -L" .. libjpeg_ROOT .. "/lib -ljpeg -Wl,-rpath," .. libjpeg_ROOT .. "/lib"
-   local ldflags2 = " -L" .. jasper_ROOT .. "/lib -ljasper -Wl,-rpath," .. jasper_ROOT .. "/lib"
-   local ldflags3 = " -L" .. libpng_ROOT .. "/lib -lpng -Wl,-rpath," .. libpng_ROOT .. "/lib"
-   local ldflags =  ldflags0 .. ldflags1 .. ldflags2 .. ldflags3
-   setenv("LDFLAGS", ldflags)
-end
